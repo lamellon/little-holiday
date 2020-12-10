bc0d1abfd77f429e7f26103dac337c998b58b33e
Lee Mellon <lamellon@gmail.com>
2020-12-10 19:23:52 +0000

    sentry config

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-const doAsyncWork = () => Promise.reject(new Error('Client Test 1'));
-doAsyncWork();
+
 function HomePage() {
   return <div>Welcome to Next.js!</div>;
 }
